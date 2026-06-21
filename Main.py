92de567e63833e7c8745cf50fca472fe248e1cb5
gfaus <gbgpf8@gmail.com>
2026-06-21 06:41:46 -0400

    feature, interface: Added building id entry field, rearranged options interface

--- Main.py
+++ Main.py
@@ -1,21 +1,42 @@
 import tkinter as tk
 from tkinter import ttk
 import os
+import typing
+
+OPTIONS = ("-", "OldToronto", "York", "EastYork", "NorthYork", "Etobicoke", "Scarborough")
 
 class Main(tk.Tk):
 
     def __init__(self):
+        """ Initialize the options window. """
         super(Main, self).__init__()
-        self.title("Screencapper")
-        self.geometry("400x100+0+0")
-        self.frame = ttk.Frame(self, padding = 10)
-        self.frame.grid()
-        ttk.Label(self.frame, text = "Hello world!").grid(column = 0, row = 0)
-        ttk.Button(self.frame, text = "Quit", command = self.on_quit).grid(column = 0, row = 1)
-        self.bind("<Escape>", func = lambda ev: self.on_quit())
+        self.title("APS360 Screencapper")
+        self.geometry("300x150+0+0")
         self.protocol("WM_DELETE_WINDOW", self.on_quit)
+        self.bind("<Escape>", func=lambda ev: self.on_quit())
 
-    def on_quit(self):
+        self.current_borough = tk.StringVar()
+        self.current_borough.set(OPTIONS[0])
+
+        self.building_id = tk.IntVar()
+        self.building_id.set(0)
+
+        self.frame = ttk.Frame(self, padding=10)
+        self.frame.grid()
+
+        ttk.Label(self.frame, text="Borough").grid(column=0, row=0, sticky="w", pady=5)
+
+        self.borough_om = ttk.OptionMenu(self.frame, self.current_borough, *OPTIONS, direction="below")
+        self.borough_om.config(width=12)
+        self.borough_om.grid(column=1, row=0, sticky="w")
+
+        ttk.Label(self.frame, text="Building ID").grid(column=0, row=1, sticky="w", padx=(0, 20), pady=5)
+
+        self.building_entry = ttk.Entry(self.frame, width=4)
+        self.building_entry.insert(0, str(self.building_id.get()))
+        self.building_entry.grid(column=1, row=1, sticky="w")
+
+    def on_quit(self) -> None:
         v = 0
         try:
             with open("out/number.txt", "r") as f:
@@ -29,8 +50,15 @@ class Main(tk.Tk):
             f.write(str(v))
         self.destroy()
 
-    def run(self):
+    def run(self) -> None:
         self.mainloop()
+
+    def get_current_picture_id_components(self) -> typing.Union[None, tuple[str, str]]:
+        """ Returns (borough, building_id) if the borough is set and the building id is a valid integer; otherwise, return None."""
+        v = self.current_borough.get()
+        if v == OPTIONS[0] or v not in OPTIONS:
+            return None
+        return v, f"{self.building_id.get()}"
 
 if __name__ == "__main__":
     Main().run()
